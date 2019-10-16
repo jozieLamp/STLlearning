cc1fa86ecfe3ac19208c887534edf70c585dd046
Josie Lamp <jl4rj@virginia.edu>
2019-10-16 14:02:41 -0400

    Completed loading of files with logging

--- Learning.py
+++ Learning.py
@@ -1,5 +1,6 @@
 import logging
 import numpy as np
+import re
 
 #
 # Learning for single client
@@ -14,14 +15,20 @@ class Learning:
     def runLearning(self):
         # BiFunction <double[], double[], Double> DISCRIMINATION_FUNCTION = (x, y) -> (x[0] - y[0]) / (Math.abs(x[1] + y[1]));  ###
 
+        #TODO - add some automatic re-shaping of the input data files to be in 3D and not 2D, and create labels
         labels = self.readVectorFromFile(self.labelsPath) # returns one D array of labels
         time = self.readVectorFromFile(self.timePath) # returns one D array of time
-        print("time len ", len(time))
-        print("labels len ",len(labels))
+        logging.info("Time ROWS "+ str(len(time)))
+        logging.info("Labels DEPTH " + str(len(labels)))
+        data = self.readMatrixFromFile(self.dataPath, time)  # returns 3D array of data
+        logging.info("Data Loaded\n" + '%s' % (data))
 
-        data = self.readMatrixFromFile(self.dataPath)  # returns 3D array of data
+
+    def learn(self):
+        pass
 
 
+    #Read in 1D vector from a txt file
     def readVectorFromFile(self, filePath):
         vals = []
         file = open(filePath, "r")
@@ -31,18 +38,40 @@ class Learning:
 
         return vals
 
-    def readMatrixFromFile(self, filePath):
-        rows = 1
-        cols = 0
+    #Read in 3D matrix from a txt file
+    def readMatrixFromFile(self, filePath, time):
+        #get dimensions of matrix
+        depth = 0
+        numAttributes = 0
         file = open(filePath, "r")
         for line in file:
+            numAttributes = 0
             for v in line.split(','):
-                cols+=1
-            rows+=1
+                numAttributes+=1
+            depth+=1
 
-        print(rows, cols)
-        #depth, columns, rows
-        vals = np.zeros((5, 1, 2))
+        rows = len(time)
+        cols = int(numAttributes / len(time))
 
+        # depth, columns, rows
+        vals = np.zeros((depth, cols, rows))
+        logging.info("New Data Shape " + str(vals.shape))
 
-        # print(vals)
+        #Now load data
+        file = open(filePath, "r")
+        d = 0
+        for line in file:
+            colCounter = 0
+            rowCounter = 0
+            for v in line.split(','):
+            # ruleStrings = re.split(',', line)
+                if colCounter == cols:
+                    rowCounter += 1
+                    colCounter = 0
+
+                vals[d][colCounter][rowCounter] = v
+                colCounter+= 1
+            #go to next depth
+            d += 1
+
+        return vals
--- Main.py
+++ Main.py
@@ -4,5 +4,6 @@ from Learning import Learning
 #make learning object
 l = Learning(logging.INFO, "Data/values", "Data/labels", "Data/times")
 
+
 #start learning
 l.runLearning()
